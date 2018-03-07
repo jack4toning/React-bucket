b800288c43b4cd727e515b96a8a4051651a8c657
jack4toning <18200388927@163.com>
2018-03-07 17:37:37 +0800

    3.7 office

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {Link} from 'react-router';
+import {Menu,Icon} from 'antd';
+import style from '../styles/home-layout.less';
+
+
 
 class HomeLayout extends React.Component {
     render(){
